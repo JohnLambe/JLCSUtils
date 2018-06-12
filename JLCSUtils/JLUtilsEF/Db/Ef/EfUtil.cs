08190562e491a9ad16bd6076103a649350549d66
John Bernard Lambe <jlambe@jlambe.com>
2018-06-13 00:12:13 +0100

    EFUtil 0.0.13: EfUtil.IsModified and UndoUnmodified.

--- JLCSUtils/JLUtilsEF/Db/Ef/EfUtil.cs
+++ JLCSUtils/JLUtilsEF/Db/Ef/EfUtil.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,5 +28,51 @@ namespace JohnLambe.Util.Db.Ef
         /// (This may change with future versions of Entity Framework.)
         /// </summary>
         private const string EfProxyNamespace = "System.Data.Entity.DynamicProxies";
+
+        /// <summary>
+        /// Change the state of entities in the EntityState.Modified to Unmodified if none of their fields are modified.
+        /// </summary>
+        /// <param name="ChangeTracker"></param>
+        public static void UndoUnmodified(DbChangeTracker ChangeTracker)
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(x => x.State == EntityState.Modified))
+            {
+                if (!IsModified(entry))
+                    entry.State = EntityState.Unchanged;
+            }
+
+            // Collection properties ?
+        }
+
+        /// <summary>
+        /// Tests whether the entity in the given entry is modified, i.e. its current values are different to the original values.
+        /// (This does NOT compare to the values actually currently in persistent storage - it just checks whether it is modified in this context.)
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <returns>true iff the entity is modified.</returns>
+        /// <seealso cref="IsEntityModified(DbContext, object)"/>
+        public static bool IsModified(DbEntityEntry entry)
+        {
+            bool modified = false;
+            foreach (var propertyName in entry.OriginalValues.PropertyNames)
+            {
+                var oldValue = entry.OriginalValues.GetValue<object>(propertyName);
+                var newValue = entry.OriginalValues.GetValue<object>(propertyName);
+                modified = modified || !JohnLambe.Util.ObjectUtil.CompareEqual(oldValue, newValue);
+            }
+            return modified;
+        }
+
+        /// <summary>
+        /// Tests whether the given entity is modified, i.e. its current values are different to the original values.
+        /// (This does NOT compare to the values actually currently in persistent storage - it just checks whether it is modified in this context.)
+        /// </summary>
+        /// <param name="context">The DbContext of the given entity.</param>
+        /// <param name="entity">The entity to test.</param>
+        /// <returns>true iff the entity is modified.</returns>
+        public static bool IsEntityModified(DbContext context, object entity)
+        {
+            return IsModified(context.Entry(entity));
+        }
     }
 }
---
+++ JLCSUtils/JLUtilsEFTest/Db/SaveNoChanges.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JohnLambe.Util.Db.Ef;
+using JohnLambe.Util.Collections;
+
+namespace JLUtilsEFTest.Db
+{
+    [TestClass]
+    public class SaveNoChanges
+    {
+        /// <summary>
+        /// Test firing of <see cref="IEntityBeforeSaveChanges.BeforeSaveChanges"/>.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Db")]
+        //[Timeout(5000)]
+        public void SaveWithNoChanges()
+        {
+            // Arrange:
+            EntityEvents.RegisterWith(_dbContext);
+
+            var dbSet = _dbContext.Set<TestEntity>();
+
+            var obj = dbSet.Find(123456);
+            if (obj == null)
+            {
+                var b = new TestEntity() { Id = 123456, Name = "No Changes Test" };
+                //byte[] rowVersion1 = b.RowVersion;
+                dbSet.Add(b);
+                _dbContext.SaveChanges();
+
+                //Assert.AreNotEqual(rowVersion1, obj.RowVersion);
+
+                obj = dbSet.Find(b.Id);
+            }
+
+            byte[] rowVersion = (byte[])obj.RowVersion.Clone();
+
+            // Act:
+            _dbContext.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+            _dbContext.SaveChanges();
+
+            // Assert:
+            Assert.AreEqual(CollectionUtil.CollectionToString(rowVersion), CollectionUtil.CollectionToString(obj.RowVersion));  // check that RowVersion has not changed
+        }
+
+        TestDbContext _dbContext = new TestDbContext();
+    }
+}
