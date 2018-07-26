6235079fa1975ebae8378acfa49db3015502e24a
John Bernard Lambe <jlambe@jlambe.com>
2018-07-26 08:37:37 +0100

    Unfinished EfUtil methods.

--- JLCSUtils/JLUtilsEF/Db/Ef/EfUtil.cs
+++ JLCSUtils/JLUtilsEF/Db/Ef/EfUtil.cs
@@ -116,13 +116,76 @@ namespace JohnLambe.Util.Db.Ef
         /// </summary>
         /// <param name="context"></param>
         /// <param name="isNewDelegate">Delegate to return true if the given object is new (not yet added).</param>
-        public static void AdjustStates(DbContext context, Func<object,bool> isNewDelegate)
+        /// <param name="isDuplicateDelegate">Delegate to return true if the given objects are for the same entity.</param>
+        public static void AdjustStates(DbContext context, Func<object,bool> isNewDelegate, Func<object,object,bool> isDuplicateDelegate = null)
         {
-            foreach (var e in context.ChangeTracker.Entries())
+            var entries = context.ChangeTracker.Entries().ToList();
+            foreach (var e in entries)
             {
                 if (e.State == System.Data.Entity.EntityState.Added && !isNewDelegate(e.Entity))
+                {
+                    if (isDuplicateDelegate != null)
+                    {
+                        foreach (var entry in context.ChangeTracker.Entries().Where(x => x.Entity != e.Entity && isDuplicateDelegate(x.Entity, e.Entity)))
+                        {
+//                            entry.State = EntityState.Detached;
+                            //TODO: Find all references to x.Entity in the context and change to e.Entity.
+                        }
+                    }
                     e.State = System.Data.Entity.EntityState.Unchanged;
+                }
             }
         }
+
+        [return: Nullable]
+        public static T CopyToContext<T>(DbContext destinationContext, T entity, EntityState newState = EntityState.Unchanged)
+            where T: class
+        {
+            if (entity == null)
+            {
+                return null;
+            }
+            else
+            {
+                var dest = EntityCloner.Clone(entity, true);
+                destinationContext.Entry(dest).State = newState;
+                return dest;
+            }
+        }
+
+        [return: Nullable]
+        public static T CopyOrFindInContext<T>(DbContext destinationContext, T entity, GetKeyDelegate getKeyDelegate, EntityState newState = EntityState.Unchanged)
+            where T : class
+        {
+            if (entity == null)
+            {
+                return null;
+            }
+            else
+            {
+                return FindInContext<T>(destinationContext, getKeyDelegate(entity), getKeyDelegate)
+                    ?? CopyToContext(destinationContext, entity, newState);
+            }
+        }
+
+        /// <summary>
+        /// Find an entity in the context by its type and key.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <param name="key"></param>
+        /// <param name="getKeyDelegate"></param>
+        /// <returns>The matching entity, or null if it is not found in the context.</returns>
+        [return: Nullable]
+        public static T FindInContext<T>(DbContext context, object key, GetKeyDelegate getKeyDelegate)
+            where T: class
+        {
+            if (key == null)
+                return null;
+            else
+                return context.ChangeTracker.Entries<T>().Where( e => getKeyDelegate(e) == key).FirstOrDefault()?.Entity;
+        }
+
+        public delegate object GetKeyDelegate(object key);
     }
 }
